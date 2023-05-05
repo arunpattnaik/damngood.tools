0a923a3651a543bf450438ef01160f607585735b
Dmytro Krasun <krasun.net@gmail.com>
2023-05-05 14:20:58 +0300

    Fix grid for small screens for the screenshot tool

--- app/tools/screenshots-for-dimensions/Screenshots.tsx
+++ app/tools/screenshots-for-dimensions/Screenshots.tsx
@@ -178,7 +178,7 @@ export function Screenshots({
                     )}
                 </Button>
             </form>
-            <div className="mt-10 grid grid-cols-3 gap-10">
+            <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
                 {screenshots.map((s) => (
                     <Screenshot
                         key={s.url}
